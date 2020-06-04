34bd3258b4616b3bab790809c9372273ca8b9f1f
unknown <ur.amirbin@gmail.com>
2020-06-04 21:56:06 +0430

    added pro tokenizer - wip

--- normalizer.py
+++ normalizer.py
@@ -9,12 +9,12 @@ def normilize(contents):
         contents[i] = re.sub('أ', 'ا', contents[i])
         contents[i] = re.sub('ؤ', 'و', contents[i])
         freaking_erab =     ['\u064B',
-                            '\u064C', 
-                            '\u064D', 
-                            '\u064E', 
-                            '\u064F', 
-                            '\u0650', 
-                            '\u0651', 
+                            '\u064C',
+                            '\u064D',
+                            '\u064E',
+                            '\u064F',
+                            '\u0650',
+                            '\u0651',
                             '\u0652']
         rx = '[' + re.escape(''.join(freaking_erab)) + ']'
         contents[i] = re.sub(rx, '', contents[i])
@@ -23,10 +23,10 @@ def normilize(contents):
 def cleanup(contents):
     for i in range(len(contents)):
         contents[i] = re.sub('<.*?>', ' ', contents[i])
-        contents[i] = re.sub('\u200c', ' ', contents[i])
         contents[i] = re.sub('\u200f', ' ', contents[i])
         contents[i] = re.sub('\n', ' ', contents[i])
         contents[i] = re.sub(r'\&.+;', ' ',contents[i])
         contents[i] = re.sub('[؟.)();:!/،,;۲۶۷۵۰۳۴۸۱۹؛1234567890]', '', contents[i])
+        contents[i] = re.sub('\u200c', ' ', contents[i])
         contents[i] = re.sub(' +', ' ', contents[i])
     return contents
--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,7 @@
 import re
 from normalizer import normilize
 from normalizer import cleanup
+import os
 
 def tokenize_by_space(contents):
     tokens = []
@@ -8,6 +9,55 @@ def tokenize_by_space(contents):
         tokens.extend(content.split(" "))
     tokens.remove('')
     tokens = list(dict.fromkeys(tokens))
+    return tokens
+
+def generate_verb_dictionary():
+    verbDic = []
+    result = []
+    resourceName = os.path.dirname(__file__) + "/PersianStemmerLib/data/VerbList.fa"
+    with open(resourceName, 'r', encoding="utf-8") as reader:
+        result = [line.strip("\r\n ") for line in reader if line.strip("\r\n ")]
+    lines = result
+    for line in lines:
+        arr = line.split("\t")
+        verbDic.append(arr[0].strip())
+    return verbDic
+
+def tokenize_look_ahead(contents):
+    verbDic = generate_verb_dictionary()
+    plural_and_the_boys = ["ها", "تر", "ترین", "ام", "ات", "اش"]
+    tokens = []
+    for i in range(len(contents)):
+        words = contents[i].split()
+        j = 0
+        while j < len(words):
+            if j < len(words) - 2:
+                if words[j] + ' ' + words[j + 1] + ' ' + words[j + 2] in verbDic:
+                    tokens.append(words[j] + ' ' + words[j + 1] + ' ' + words[j + 2])
+                    j+=2
+                elif 
+                elif words[j] + ' ' + words[j + 1] in verbDic:
+                    tokens.append(words[j] + ' ' + words[j + 1])
+                    j+=1
+                elif words[j + 1] in plural_and_the_boys:
+                    tokens.append(words[j] + ' ' + words[j + 1])
+                    j+=1
+                else:
+                    tokens.append(words[j])
+                j+=1
+            elif j < len(words) - 1:
+                if words[j] + ' ' + words[j + 1] in verbDic:
+                    tokens.append(words[j] + ' ' + words[j + 1])
+                    j+=1
+                elif words[j + 1] in plural_and_the_boys:
+                    tokens.append(words[j] + ' ' + words[j + 1])
+                    j+=1
+                else:
+                    tokens.append(words[j])
+                j+=1
+            else:
+                tokens.append(words[j])
+                j+=1
     return tokens
 
 def remove_frequent_tokens(tokens):
@@ -24,7 +74,9 @@ def simple_tokenize(contents):
 
 def pro_tokenizer(contents):
     contents = cleanup(contents)
-    contents = normalize(contents)
+    tokens = tokenize_look_ahead(contents)
+    tokens = remove_frequent_tokens(tokens)
+    return tokens
 
 def tokenize(tokenizer_type, contents):
     if tokenizer_type == 'simple':
